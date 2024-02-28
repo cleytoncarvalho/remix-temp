ca150880b0621c2b2fddbac0768279eb8abd777d
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2024-02-28 11:41:40 -0300

    all

--- app/routes/success.tsx
+++ app/routes/success.tsx
@@ -1,7 +1,11 @@
+import { Link } from "@remix-run/react";
+
 export default function Success() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Success</h1>
+
+      <Link to="/">Go back</Link>
     </div>
   );
 }
